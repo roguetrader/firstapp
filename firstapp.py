4b6dfb530131ba73a3997b7bcfebac43fe28e2d4
Matt <matt@sam-n-matt.co.uk>
2021-02-09 16:34:18 +0000

    new file

--- firstapp.py
+++ firstapp.py
@@ -1,8 +1,23 @@
+import feedparser
+
 from flask import Flask
 
 app = Flask(__name__)
 
+wsb = 'https://reddit.com/r/wallstreetbets/.rss'
+
 @app.route("/")
-def hello_world():
-	return "Hello to the Barton family web site"
+def Get_feed():
+        d=feedparser.parse(wsb)
+        first_article = d['entries'][0]
+        return  """<html>
+		<body><h1> WallStreet Bets Reddit</h1>
+			<b>{0}</b></br>
+			<i>{1}</i></br>
+			<p>{2}</p>
+		</body>
+	</html>""".format(first_article.get('title'), first_article.get('summary'),first_article.get('subtitle'))
+
+if __name__ == '__main__':
+        app.run(host='0,0,0,0' debug=True)
 
