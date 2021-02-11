baaf0acc5636364d96608e0032c48a5b545bb85f
Matt <matt@sam-n-matt.co.uk>
2021-02-11 16:34:12 +0000

    small change

--- firstapp.py
+++ firstapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 		'cnn':'http://rss.cnn.com/rss/edition.rss',
 		'iol':'https://rss.iol.io/iol/news'
-		'iol':'http://feeds.foxnews.com/foxnews/latest'}
+		'fox':'http://feeds.foxnews.com/foxnews/latest'}
 
 @app.route("/")
 @app.route("/bbc")
