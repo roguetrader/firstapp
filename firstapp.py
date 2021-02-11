bfb8459b7a08f6e9d3eaedad750ce6218a58fd47
Matt <matt@sam-n-matt.co.uk>
2021-02-11 18:07:31 +0000

    added Jinja templates

--- firstapp.py
+++ firstapp.py
@@ -1,10 +1,10 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-#wsb = 'https://reddit.com/r/wallstreetbets/.rss'
 RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 		'cnn':'http://rss.cnn.com/rss/edition.rss',
 		'iol':'https://rss.iol.io/iol/news',
@@ -15,13 +15,7 @@ RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
         feed=feedparser.parse(RSS_FEEDS[publication])
         first_article = feed['entries'][0]
-       	return  """<html>
-		<body><h1> Feed Parsing</h1>
-			<b>{0}</b></br>
-			<i>{1}</i></br>
-			<p>{2}</p>
-		</body>
-	</html>""".format(first_article.get('title'), first_article.get('published'),first_article.get('summary'))
+       	return  render_template(home.html)
 
 if __name__ == '__main__':
         app.run()
