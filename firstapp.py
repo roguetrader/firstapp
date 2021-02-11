066aef30bbaa7e752c7008eae3fae7757a45c08b
Matt <matt@sam-n-matt.co.uk>
2021-02-11 19:40:29 +0000

    added Jinja templates

--- firstapp.py
+++ firstapp.py
@@ -20,7 +20,7 @@ RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
         feed=feedparser.parse(RSS_FEEDS[publication])
         first_article = feed['entries'][0]
-       	return  render_template("home.html", article=first_article)
+       	return  render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
         app.run()
