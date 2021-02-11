cd58d17a4d3c71f378235150b25edd604abf491c
Matt <matt@sam-n-matt.co.uk>
2021-02-11 17:10:47 +0000

    small change

--- firstapp.py
+++ firstapp.py
@@ -11,23 +11,8 @@ RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 		'fox':'http://feeds.foxnews.com/foxnews/latest'}
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-	return get_news('bbc')
-
-@app.route("/fox")
-def fox():
-	return get_news('fox')
-
-@app.route("/iol")
-def iol():
-	return get_news('iol')
-
-@app.route("/cnn")
-def cnn():
-	return get_news('cnn')
-
-def get_news(publication):
+@app.route("/<publication>")
+def get_news(publication="bbc"):
         feed=feedparser.parse(RSS_FEEDS[publication])
         first_article = feed['entries'][0]
        	return  """<html>
