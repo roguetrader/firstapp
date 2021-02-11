f5fc6f5a62519e9fd8135bf532b3b29c83316410
Matt <matt@sam-n-matt.co.uk>
2021-02-11 15:42:39 +0000

    small change

--- firstapp.py
+++ firstapp.py
@@ -10,15 +10,15 @@ wsb = 'https://feeds.bbci.co.uk/news/rss.xml'
 @app.route("/")
 def Get_feed():
         d=feedparser.parse(wsb)
-	for i range(10):
-	        first_article = d['entries'][i]
-        		return  """<html>
-			<body><h1> Feed Parsing</h1>
-				<b>{0}</b></br>
-				<i>{1}</i></br>
-				<p>{2}</p>
-			</body>
-		</html>""".format(first_article.get('title'), first_article.get('published'),first_article.get('summary'))
+
+        first_article = d['entries'][0]
+       		return  """<html>
+		<body><h1> Feed Parsing</h1>
+			<b>{0}</b></br>
+			<i>{1}</i></br>
+			<p>{2}</p>
+		</body>
+	</html>""".format(first_article.get('title'), first_article.get('published'),first_article.get('summary'))
 
 if __name__ == '__main__':
         app.run()
