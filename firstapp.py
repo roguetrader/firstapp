a946d0c6b5566416c0f7145101b35073ebb57dba
Matt <matt@sam-n-matt.co.uk>
2021-02-12 16:27:41 +0000

    added Jinja templates

--- firstapp.py
+++ firstapp.py
@@ -1,7 +1,7 @@
 import feedparser
-
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -17,9 +17,14 @@ RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml',
 		'btc':'https://medium.com/feed/the-capital'}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-        feed=feedparser.parse(RSS_FEEDS[publication])
+def get_news():
+	query = request.args.get("publication")
+	if not query or query.lower() not in RSS_FEEDS:
+		publication = "bbc"
+	else:
+		publication  = query.lower()
+
+	feed=feedparser.parse(RSS_FEEDS[publication])
         first_article = feed['entries'][0]
        	return  render_template("home.html", articles=feed['entries'])
 
